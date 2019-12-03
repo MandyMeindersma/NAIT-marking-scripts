fa99bb0ebd0c369bc40ff08602322099bd238425
Mandy Meindersma <meinders@ualberta.ca>
2019-12-03 08:47:18 -0700

    script to unzip files is working

--- unzipper.py
+++ unzipper.py
@@ -1,3 +1,13 @@
 import zipfile
-with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
-    zip_ref.extractall(directory_to_extract_to)
+import os
+
+NAME_OF_STUDENT_ZIP = 'COMP1017.1191.A10.REG-Assignment-3-6805491'
+
+rootDir = 'C:/users/meind/dev/marking-scripts/'+ NAME_OF_STUDENT_ZIP
+for dirName, subdirList, fileList in os.walk(rootDir):
+    if dirName[-5:] == "file_":
+        print(dirName.split("\\")[-1].split("_")[0])
+        for fname in fileList:
+            print('\t%s' % fname)
+            with zipfile.ZipFile(dirName+"/" +fname, 'r') as zip_ref:
+                zip_ref.extractall(dirName)
